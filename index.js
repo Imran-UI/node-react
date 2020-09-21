16ea6861af113fd08a9c2ceb8a7fd5c1647e98fc
Imran Shaik <mshaik4@cisco.com>
2020-09-20 23:33:25 -0400

    added passport js + google oauth

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const keys = require("./config/keys");
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GoogleClientID,
+      clientSecret: keys.GoogleClientSecret,
+      callbackURL: "http://localhost:5000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log("accessToken ", accessToken);
+      console.log("refreshToken ", refreshToken);
+      console.log("profile ", profile);
+    }
+  )
+);
 
-app.get('/', (req, resp) => {
-    resp.send({Hi : 'Hello Afsha!!, Imran Loves You'});
-})
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-const PORT = process.env.PORT || 5000
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
