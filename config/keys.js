acb8e99dce01606b0df93bf0d55f02ee2279efba
Imran Shaik <mshaik4@cisco.com>
2020-09-22 23:53:29 -0400

    typo fix

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
   // we are in prod 
-  module.exports = require('./dev');
+  module.exports = require('./prod');
 }else {
   //we are in dev 
   module.exports = require('./dev');
